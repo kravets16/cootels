684e81c26ea675e080d7b79bc6034eb8b2b7ad22
unknown <kravets16@gmail.com>
2022-05-06 18:48:58 +0300

    add customers section

---
+++ src/js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('.customers__carousel').slick({
+        arrows: false,
+        infinite: true,
+        dots: true,
+        //autoplay: true,
+        //autoplaySpeed: 3000,
+        speed: 1500,
+        adaptiveHeight: true,
+        pauseOnHover: true
+      });
+  });
